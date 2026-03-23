cadefb7692a5ac10fbb1ee97d82a3fb09b1d4234
CGomesLucas <cruzlucasads1@gmail.com>
2026-03-23 08:59:07 -0300

    refactor: Ajuste de lógica Exerc 4 Listas

--- Exercícios sobre listas/Exercicio4/Exerc4.py
+++ Exercícios sobre listas/Exercicio4/Exerc4.py
@@ -1,7 +1,12 @@
 letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
-
-# não imprime a, e, i
+qtd_consoantes = 0
+letras_consoantes = []
 
 for letra in letras:
     if letra != 'A' and letra != 'E' and letra != 'I' and letra != 'O' and letra != 'U':
-        print(letra)
+        letras_consoantes += letra
+
+print(letras_consoantes) # não imprime a, e, i
+print(f"Foram lidas {len(letras_consoantes)} consoantes")
+
+
