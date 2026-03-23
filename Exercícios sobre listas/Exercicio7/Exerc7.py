f1a5f64d75791e5ae9c72027618c898a0105edcb
CGomesLucas <cruzlucasads1@gmail.com>
2026-03-23 09:26:21 -0300

    Exerc 7 Listas

---
+++ Exercícios sobre listas/Exercicio7/Exerc7.py
@@ -0,0 +1,13 @@
+import math
+
+numeros = [5, 10, 15, 20, 25]
+
+soma = sum(numeros)
+multiplicacao = math.prod(numeros)
+
+print(numeros)
+print("A soma dos números da lista é:",soma)
+print(f"A multiplicação dos números da lista é:",multiplicacao)
+
+
+
