87a32feac1387eddfee7b73a25bbc3497295831b
CGomesLucas <cruzlucasads1@gmail.com>
2026-03-23 08:52:22 -0300

    Exerc 4 Listas

---
+++ Exercícios sobre listas/Exercicio4/Exerc4.py
@@ -0,0 +1,7 @@
+letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+
+# não imprime a, e, i
+
+for letra in letras:
+    if letra != 'A' and letra != 'E' and letra != 'I' and letra != 'O' and letra != 'U':
+        print(letra)
