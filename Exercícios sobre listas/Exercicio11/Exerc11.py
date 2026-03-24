c8cd7d71a040a09574fe2dc8b8b51f6c22841d94
CGomesLucas <cruzlucasads1@gmail.com>
2026-03-24 10:51:23 -0300

    Exerc 11 Listas

---
+++ Exercícios sobre listas/Exercicio11/Exerc11.py
@@ -0,0 +1,6 @@
+primeira_lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+segunda_lista = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+terceira_lista = [21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+
+quarta_lista = primeira_lista + segunda_lista + terceira_lista
+print(quarta_lista)
